ea75f687893dae6dd4a95aaf865d1835abeee2c9
Salma Ben <benbrahimsalma18@gmail.com>
2024-02-03 14:54:17 +0100

    version 8

--- change.js
+++ change.js
@@ -1,2 +1,2 @@
 console.log('im practising git and GitHub for the first time');
-console.log("version4");
+console.log("version8");
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('im practising git and GitHub for the first time');
-console.log("version3");
+console.log("version6");
